935458472f19b19eb2d2eae27ccd1f8098d9053f
Serge-Olivier Amega <sergeolivieramega@gmail.com>
2017-12-20 01:27:16 -0500

    implement relative filenames with @

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,29 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_yaml;
+extern crate regex;
 
 mod engine;
 mod utilities;
 
 use std::env;
 use std::fs::File;
+use std::path::Path;
 use std::io::Read;
+
+use self::regex::Regex;
 
 use engine::renderer::Config;
 
-pub fn load_yml_config_from_string(text: &str) -> Result<Config, serde_yaml::Error> {
-    let config: Config = serde_yaml::from_str(text).unwrap();
+pub fn load_yml_config_from_string(directory_prefix: &str, text: &str)
+                                   -> Result<Config, serde_yaml::Error>
+{
+    let re = Regex::new(r"'@/(?P<path>.*)'").unwrap();
+    let text_with_relative_dirs: String = re.replace_all(
+        text,
+        format!("{}/$path", directory_prefix).as_str()
+    ).into();
+    let config: Config = serde_yaml::from_str(text_with_relative_dirs.as_str()).unwrap();
     Ok(config)
 }
 
@@ -22,14 +33,18 @@ fn main() {
     let mut arguments = env::args();
     arguments.next();
     let filename = arguments.next().expect("no filename provided");
+    let filepath = Path::new(filename.as_str());
 
-    let mut file = File::open(filename).unwrap();
+    let mut file = File::open(filepath).unwrap();
     let mut s = String::new();
     file.read_to_string(&mut s).unwrap();
 
+    let directory_string = filepath.parent().unwrap()
+        .to_str().unwrap();
+
     //load scene
     //let render_config = config_loader::load_config_from_string(s.as_str()).unwrap();
-    let render_config = load_yml_config_from_string(s.as_str())
+    let render_config = load_yml_config_from_string(directory_string, s.as_str())
         .unwrap();
     
     let buffer = render_config.render();
